6b6b713d3503a4a6a1567dc99a004492f831e3e4
chinmaya255696 <chinmayasahoo.v@gmail.com>
2023-01-24 17:54:33 +0530

    final commit my-cv button

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -8,21 +8,27 @@ const HeroImg = () => {
     <div className="hero">
       <div className="mask">
         <img className="into-img" src={IntroImg} alt="IntroImg" />
-        </div>
-        <div className="content">
-          <p>HI, I'M Chinmaya Sahoo</p>
-          <h1> A Backend Devloper.</h1>
-          <div>
-            <Link to="/project" className="btn">
-              Projects 
-            </Link>
-            <Link to="/contact" className="btn btn-light">
-              Contact
-            </Link>
-          </div>
+      </div>
+      <div className="content">
+        <p>HI, I'M Chinmaya Sahoo</p>
+        <h1> A Backend Devloper.</h1>
+        <div >
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+
+          <a
+            href="https://drive.google.com/uc?export=download&id=1Tu6oPyo5x5QQsLaplXNkrUJIOS8d3AlN"
+            className="btn btn-cv"
+          >
+            My-CV
+          </a>
         </div>
       </div>
-
+    </div>
   );
 };
 
